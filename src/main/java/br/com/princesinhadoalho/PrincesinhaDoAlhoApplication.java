1b5303cec06775613ebc938f70c457cc8bb1a654
EricRangelRJ <b.ericrangel@hotmail.com>
2021-10-16 17:32:37 -0300

    Inicial

---
+++ src/main/java/br/com/princesinhadoalho/PrincesinhaDoAlhoApplication.java
@@ -0,0 +1,13 @@
+package br.com.princesinhadoalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincesinhaDoAlhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincesinhaDoAlhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/princesinhadoalho/PrincesinhaDoAlhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.princesinhadoalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincesinhaDoAlhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
