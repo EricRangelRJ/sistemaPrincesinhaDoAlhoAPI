db4a01b5e5cf06c11735b969b4fdf63ee3572e40
EricRangelRJ <b.ericrangel@hotmail.com>
2021-10-19 09:59:06 -0300

    INstalacao dependencias Lombok e criacao da entidade cliente

--- src/main/java/br/com/princesinhadoalho/entities/Cliente.java
+++ src/main/java/br/com/princesinhadoalho/entities/Cliente.java
@@ -1,5 +1,43 @@
 package br.com.princesinhadoalho.entities;
 
-public class Cliente {
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 5853510303381995346L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer idCliente;
+
+	@Column(length = 100, nullable = false)
+	private String nome;
+
+	@Column(nullable = false)
+	private Date dataNascimento;
+
+	@Column(length = 20, unique = true)
+	private String cpf;
 
 }
